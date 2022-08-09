627a2cf39a1aaf38eb022a23a0bf5c960e1759ff
Israel Schroeder Moreira <israelschroederm@gmail.com>
2022-08-09 11:38:23 -0300

    Perguntas e Respostas
    Adicionado um exercício de perguntas e respostas

---
+++ JavaScript Puro/9) Exercício - Perguntas e respostas/script.js
@@ -0,0 +1,50 @@
+/*
+to do list
+
+1) colocar span e input type radio dentro de uma div pra preencher toda a linha ✔
+2) recuperar o elemento pai da opcao correta ou incorreta e aplicara a cor certa ✔
+3) depois de selecionado a resposta deixar o botao desabilitado ✔
+
+*/
+
+function DisableOptions() {
+    // função que desabilita as opções de radio após uma ser selecionada
+    let els = parentNode.parentNode.querySelectorAll('[type=radio]');
+    for (let c = 0; c < els.length; c++) {
+        els[c].disabled = true;
+    }
+}
+
+var elementos = document.querySelectorAll('[type=radio]');
+
+for (let i = 0; i < elementos.length; i++) {
+    elementos[i].addEventListener('change', (e) => {
+        let marcado = e.target.value;
+        if (marcado == "correto") {
+
+            //muda o background para verde se for correto
+            let parentNode = e.target.parentNode;
+            parentNode.style.backgroundColor = "green";
+
+            //desabilitar os outros radios apos selecionar uma
+            DisableOptions();
+
+            console.log("você acertou");
+        } else if (marcado == "incorreto") {
+
+            //muda o background para vermelho se for incorreto
+            let parentNode = e.target.parentNode;
+            parentNode.style.backgroundColor = "red";
+
+            //desabilitar os outros radios apos selecionar uma
+            DisableOptions();
+
+            //se incorreto, mostra a alternativa correta
+            let correta = parentNode.parentNode.querySelector('[value=correto]');
+            correta.parentNode.style.backgroundColor = 'green';
+
+            console.log("você errou");
+        }
+    })
+
+}
